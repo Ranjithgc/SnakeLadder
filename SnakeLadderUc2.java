1815a175f4427b0fa2d2203062639ba2a12da74c
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-05-25 13:19:59 +0530

    Adding Snake Ladder Uc2

---
+++ SnakeLadderUc2.java
@@ -0,0 +1,12 @@
+public class SnakeLadderUc2
+{
+	public static void main(String[] args)
+	{
+		int random_check = (int)Math.floor(Math.random() * 10) % 7;
+	
+		if(random_check != 0)
+		{
+			System.out.println(random_check);
+		}
+	}
+}
